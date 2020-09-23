8d1eb73fda24bb32828def8a0bee2365e7ca7e9d
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2020-09-24 00:43:42 +0200

    - Created the main bot nav - Added icons - Added listener on the bot nav

--- app/src/main/java/com/example/achievekotlin/MainActivity.kt
+++ app/src/main/java/com/example/achievekotlin/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.achievekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var textView: TextView
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+             //   moveToFragment(HomeFragment())
+                textView.setText("Home")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add -> {
+             //   moveToFragment(SearchFragment())
+                textView.setText("Add")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_graph -> {
+                //   moveToFragment(SearchFragment())
+                textView.setText("graph")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        textView.findViewById<TextView>(R.id.message)
     }
 }
